9f3549cf416a2a7d641bf8ae1a0bcdc83458a4bc
Mete <muyusuon@gmail.com>
2025-11-18 15:56:31 +0300

    Veri tabanı - Veri tabanı entegre edildi (SQLite) - Ekleme ve gösterme işlemi tamamlandı.

--- App.js
+++ App.js
@@ -5,10 +5,61 @@ import { StyleSheet } from 'react-native';
 
 import HomePage from './pages/homepage/homepage.js'
 import Dashboard from './pages/dashboard/dashboard.js'
+import { useEffect } from 'react';
+
+import * as SQLite from "expo-sqlite";
 
 const Tab = createBottomTabNavigator();
 
+async function initDB() {
+    // Kütüphane nesnesinin varlığını kontrol edin
+    if (!SQLite) {
+        console.log("SQLite kütüphanesi yüklenemedi!");
+        return; 
+    }
+    
+    const db = SQLite.openDatabaseSync("test.db");
+
+    if(db)
+      console.log("isokay")
+
+    // await db.transaction((query) => {
+    //   query.executeSql(
+    //     "INSERT INTO FocussingTracking (Time, Date, Category, TotalDistractions) VALUES (1500, '2025-11-18', 'project', 5)"
+    //   )
+    // })
+};
+
+function CreateTable(){
+  const db = SQLite.openDatabaseSync("test.db");
+
+  db.execSync(
+    "CREATE TABLE IF NOT EXISTS FocussingTracking "+
+    "(id INTEGER PRIMARY KEY AUTOINCREMENT, Time INTEGER, Date TEXT, Category TEXT, TotalDistractions INTEGER);"
+  )
+
+  console.log("İyi gidiyoruz") 
+}
+
+function CreateItems(time, date, category, totalDistractions){
+  const db = SQLite.openDatabaseSync("test.db");
+
+  db.runSync(
+    "INSERT INTO FocussingTracking (Time, Date, Category, TotalDistractions) VALUES"+
+    " (?, ?, ?, ?);", [time, date, category, totalDistractions]
+  )
+}
+
 export default function TabMenu() {
+
+  useEffect(() => {
+    initDB()
+    CreateTable()
+    //CreateItems(1500, new Date(2025, 11, 18, 2, 30, 0).toString(), "project", 5)
+  }, [])
+
+
+
   return (
     <NavigationContainer>
       <Tab.Navigator
--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -1,20 +1,36 @@
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
-import { ScrollView } from 'react-native-web';
+import { StyleSheet, View, TouchableOpacity, Text, ScrollView, Dimensions } from 'react-native';
 
 import TotalFocussing from './statistics/TotalFocussing.js';
 import AllTimeFocussing from './statistics/AllTimeFocussing.js';
 import TotalDistraction from './statistics/TotalDistraction.js';
-import { useState } from 'react';
+
+import { DataTable } from 'react-native-paper'
+import { useEffect, useState } from 'react';
+
+import * as SQLite from "expo-sqlite";
 
 export default function DashboardScreen(){
 
   const [page, setPage] = useState(0)
+  const [rows, setRows] = useState([])
+
+  useEffect(() => {
+    const db = SQLite.openDatabaseSync("test.db");
+
+    const dneeme = db.getAllSync("SELECT * FROM 'FocussingTracking'")
+    console.log(dneeme)
+
+    setRows(db.getAllSync("SELECT * FROM 'FocussingTracking'"))
+  }, [])
+
   const up = "<--"
   const down = "-->"
 
+  const width = Dimensions.get("window").width
+
   return (
     <View style={styles.tab}>
-      <ScrollView style={styles.scroll}>
+      <ScrollView style={{flex: 1, width: width}}>
         <View style={styles.tables}>
           {
             page === 0 ? <TotalFocussing /> :
@@ -56,7 +72,28 @@ export default function DashboardScreen(){
           </TouchableOpacity>
 
         </View>
+        
+        {/* <View style={styles.FocussingTracking}>
 
+          <DataTable>
+            <DataTable.Header>
+                <DataTable.Title style={styles.title}>Kategori</DataTable.Title>
+                <DataTable.Title style={styles.title}>Süre</DataTable.Title>
+                <DataTable.Title style={styles.title}>Tarih</DataTable.Title>
+                <DataTable.Title style={styles.title}>TotalDistraction</DataTable.Title>
+            </DataTable.Header>
+            {
+              rows.map(item => (
+                <DataTable.Row>
+                    <DataTable.Cell>{item.Category}</DataTable.Cell>
+                    <DataTable.Cell>{item.Time}</DataTable.Cell>
+                    <DataTable.Cell>{item.Date}</DataTable.Cell>
+                    <DataTable.Cell>{item.TotalDistractions}</DataTable.Cell>
+                </DataTable.Row>
+              ))
+            }
+          </DataTable>
+        </View> */}
       </ScrollView>
     </View>  
   )
@@ -69,12 +106,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'white'
   },
-  scroll: {
-    flex: 1,
-    width: window.screen.width
-  },
   tables: {
     flexDirection: 'row',
+    padding: 15
+  },
+  FocussingTracking: {
+    marginTop: 50,
+    flex: 5,
     padding: 15
   },
   ChangePage: {
